90911ab0cb9629c160a56c32d524584cda206976
Aandre <160123777+AndreaPesent@users.noreply.github.com>
2025-11-13 09:31:32 +0100

    Update LoginApp.php

--- LoginApp.php
+++ LoginApp.php
@@ -1,39 +1,33 @@
 <?php
-    session_start();
-    $file = 'users.json';
-    if (file_exists($file)) {
-    $users = json_decode(file_get_contents($file), true);
-    if (!$users) {
-        $users = array();
-    }
-} else {
-    $users = array();
-}
+session_start();
+$file = 'users.json';
+$users = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
 $message = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
-
-    if ($username == "" || $password == "") {
-        $message = "Inserisci username e password";
+    if ($username === "" || $password === "") 
+    {
+        $message = "Inserisci username e password.";
     } elseif (!isset($users[$username])) {
-        $message = "Utente non registrato, <a href='register.php'>registrati qui</a>";
-    } elseif ($users[$username] != $password) {
-        $message = "Password sbagliata.";
+        $message = "Utente non registrato. <a href='Registrazione.php'>Registrati qui</a>";
+    } elseif ($users[$username] !== $password) 
+    { 
+        $message = "Password errata.";
     } else {
         $_SESSION['username'] = $username;
-        echo "Accesso effettuato come ". $_SESSION['username'];
+        header("Location: Persona.php");
         exit();
     }
 }
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
+<html lang="it">
+<head><meta charset="UTF-8"><title>Login</title></head>
 <body>
 <h1>Login</h1>
+<?php if ($message): ?><p><b><?= htmlspecialchars($message) ?></b></p><?php endif; ?>
 <form method="post">
     Username: <input type="text" name="username" required><br><br>
     Password: <input type="password" name="password" required><br><br>
